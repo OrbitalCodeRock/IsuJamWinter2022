37337c06f1c4e83af30517bfd4a76710d918d221
Daniel Beyer <dbeyer180@gmail.com>
2022-12-31 00:29:11 -0600

    Started working on controllable units

---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+    public float armor;
+    public float speed;
+    public float evasion;
+
+}
